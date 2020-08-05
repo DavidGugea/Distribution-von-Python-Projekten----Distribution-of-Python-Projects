59ef0b09a766d314f19fe6aba1bb7ab5d702124e
David <davidgugea@yahoo.com>
2020-08-05 14:48:09 +0200

    Learning how to use setuptools, how to create a source-code-distribution & how to install it

---
+++ setuptools_ChapterPart/FirstDistributionSetup/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+        name = "shuffle_word.py",
+        version = "1.0",
+        author = "Micky Maus",
+        author_email = "micky@maus.de",
+        py_modules = ["shuffle_word.py"]
+)
